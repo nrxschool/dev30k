45aef515eb477b454bf1db57929a7deefe1d1eb5
Lucas Oliveira <olivmath@protonmail.com>
2024-11-15 17:42:48 -0300

    save readme

--- playground/challenger5/tokens/script/deploy.sh
+++ playground/challenger5/tokens/script/deploy.sh
@@ -7,3 +7,14 @@ stellar contract deploy \
   --wasm target/wasm32-unknown-unknown/release/$CONTRACT_NAME.wasm \
   --source alice \
   --network local
+
+echo "👉 ENTER CONTRACT ID: "
+read CONTRACT_ID
+
+stellar contract invoke \
+  --id $CONTRACT_ID \
+  --source alice \
+  --network local \
+  -- \
+  initialize \
+  --admin $(stellar keys address alice)
